df7378244f9b7a00ca820fd13bd835bc288cb0c8
carlosberti <carlosberti182@gmail.com>
2025-02-06 15:39:13 -0300

    add clerk auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import { Inter } from "next/font/google";
 
+import { ClerkProvider } from "@clerk/nextjs";
+
 import type { Metadata } from "next";
 
 import "./globals.css";
@@ -19,8 +21,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
