e2467f51e4bb0e036717fc6e8f10a0b897d05c27
carlosberti <carlosberti182@gmail.com>
2025-02-06 16:00:05 -0300

    add clerk sign-in and sign-up pages

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,12 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+const protectedRouteMatcher = createRouteMatcher(["/protected(.*)"]);
+
+export default clerkMiddleware(async (auth, req) => {
+  if (protectedRouteMatcher(req)) {
+    await auth.protect();
+  }
+});
 
 export const config = {
   matcher: [
