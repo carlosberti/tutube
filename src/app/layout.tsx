f0becf5c26afedcaedbda48cf45ee1006782472b
carlosberti <carlosberti182@gmail.com>
2025-02-06 12:05:15 -0300

    add prettier and format code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,29 +1,25 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 
-import './globals.css'
+import "./globals.css";
 
 const inter = Inter({
-  subsets: ['latin']
-})
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
-  title: 'TuTube',
-  description: 'A YouTube clone built with Next.js and Tailwind CSS',
-}
+  title: "TuTube",
+  description: "A YouTube clone built with Next.js and Tailwind CSS",
+};
 
-export default function RootLayout ({
-  children
+export default function RootLayout({
+  children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
-      <body
-        className={inter.className}
-      >
-        {children}
-      </body>
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
