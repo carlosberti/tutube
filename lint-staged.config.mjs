8f21b2048dee8103577b1804f59f61d84dc0711b
carlosberti <carlosberti182@gmail.com>
2025-02-06 12:28:16 -0300

    add vitest

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,5 +1,9 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
-  "src/**/*.{js,jsx,ts,tsx}": ["bun prettier --write", "bun eslint --fix"],
+  "src/**/*.{js,jsx,ts,tsx}": [
+    "bun prettier --write",
+    "bun eslint --fix",
+    "bun vitest related --run"
+  ],
   "src/**/*.{json,css,md}": ["bun prettier --write"]
 }
